a9dba2610e655d96eed7e1005e402a9ab4261e50
Leonard Günther <leonard@eynck.de>
2024-09-26 18:10:00 +0200

    Expose TableControllerNotifier
    for better customizability

--- lib/paged_datatable.dart
+++ lib/paged_datatable.dart
@@ -3,6 +3,7 @@ library paged_datatable;
 export './l10n/generated/l10n.dart';
 export './src/paged_datatable.dart';
 export './src/column_size.dart';
+export './src/table_controller_notifier.dart';
 export './src/column_format.dart';
 export './src/configuration.dart';
 export './src/footer.dart';
